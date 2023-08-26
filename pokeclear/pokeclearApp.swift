bd5101d0ed582452f0eed88fd2b0650b1cb1f38a
Irfan Maulana <irfan.resimen2018@gmail.com>
2023-08-26 08:23:34 +0700

    Initial Commit

---
+++ pokeclear/pokeclearApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeclearApp.swift
+//  pokeclear
+//
+//  Created by Macbook on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct pokeclearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
